4e891ba2e14bfde4283b0f256e05757b4474f12b
wongjason <jawong1212@gmail.com>
2020-05-03 11:05:19 -0700

    added new features to recipe model

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -2,8 +2,38 @@ class Recipe {
   String recipeName;
   String recipeImageUrl;
   String recipeUrl;
+  double calories;
+  List<dynamic> ingredients;
+  double servings;
+  double time;
+  List<dynamic> healthLabel;
+  List<dynamic> dietLabel;
 
+  Recipe(
+      {this.recipeName,
+      this.recipeImageUrl,
+      this.recipeUrl,
+      this.calories,
+      this.ingredients,
+      this.servings,
+      this.time,
+      this.healthLabel,
+      this.dietLabel});
 
-  Recipe(this.recipeName, this.recipeImageUrl, this.recipeUrl);
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+        recipeName: json['recipe']['label'],
+        recipeImageUrl: json['recipe']['image'],
+        recipeUrl: json['recipe']['url'],
+        calories: json['recipe']['calories'],
+        ingredients: json['recipe']['ingredientLines'],
+        servings: json['recipe']['yield'],
+        time: json['recipe']['totalTime'],
+        healthLabel: json['recipe']['healthLabels'],
+        dietLabel: json['recipe']['dietLabels']);
+  }
 
+  String toString() {
+    return 'Recipe Name: $recipeName\n img: $recipeImageUrl\n url: $recipeUrl\n calories: $calories\n ingredients: $ingredients\n time:$time';
+  }
 }
