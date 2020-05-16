2bb70e07dd028e1ec0ad6acd3f795a2591726caa
Henry Wong <34784225+henrywong1@users.noreply.github.com>
2020-05-16 13:36:08 -0700

    fix theme primary color

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -250,8 +250,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       debugShowCheckedModeBanner: false,
       theme: isDarkTheme
-          ? ThemeData(brightness: Brightness.light, primaryColor: Colors.white, backgroundColor: Colors.white)
-          : ThemeData(brightness: Brightness.dark, primaryColor: Colors.black, backgroundColor: Colors.grey[850]),
+          ? ThemeData(brightness: Brightness.light, primaryColor: Colors.black, backgroundColor: Colors.white)
+          : ThemeData(brightness: Brightness.dark, primaryColor: Colors.white, backgroundColor: Colors.grey[850]),
     );
   }
 }
